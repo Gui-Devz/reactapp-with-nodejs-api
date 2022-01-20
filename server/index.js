5777df3751ee599e86b0c51ef27f1abc954b8433
Guilherme <guilherme.devz@hotmail.com>
2022-01-20 13:27:11 -0300

    ops

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
